e1a6ece3b9ef10969ae8738ff3d29c28832c2293
Peter Siemens <peterjsiemens@gmail.com>
2018-03-16 01:39:06 -0700

    Fix deploy settings

--- _settings/settings-dpl.py
+++ _settings/settings-dpl.py
@@ -8,12 +8,10 @@ SECRET_KEY = 'TEMP-KEY'
 
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS += ['phoenix']
-
 STATICFILES_DIRS += (
     os.path.join(os.path.dirname(__file__), 'static/dist'),
 )
 
-STATIC_ROOT = '/home/travis/build/psiemens/phoenix/gcs/static'
+STATIC_ROOT = '/home/travis/build/ubyssey/thephoenixnews.com/gcs/static'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
