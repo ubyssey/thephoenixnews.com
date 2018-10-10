a6a7bb523662b519bed4ac713a253aaf833d5321
Peter Siemens <peterjsiemens@gmail.com>
2018-10-09 20:09:06 -0700

    Version 0.0.52

--- _settings/settings-prd.py
+++ _settings/settings-prd.py
@@ -63,6 +63,7 @@ GS_SECRET_ACCESS_KEY = Secrets.get('GS_SECRET_ACCESS_KEY')
 GS_STORAGE_BUCKET_NAME = 'phoenix-news'
 GS_LOCATION = 'media'
 GS_USE_SIGNED_URLS = True
+GS_QUERYSTRING_AUTH = False
 
 STATICFILES_DIRS += (
     os.path.join(os.path.dirname(__file__), 'static/dist'),
